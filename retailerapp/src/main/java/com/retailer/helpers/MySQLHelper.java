88bbbe90c85a05c9d23a7138fcb7b7cb66162e62
eswaribala <parameswaribala@gmail.com>
2023-08-29 13:46:50 +0530

    updated

---
+++ retailerapp/src/main/java/com/retailer/helpers/MySQLHelper.java
@@ -0,0 +1,28 @@
+package com.retailer.helpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class MySQLHelper {
+	
+	private static ResourceBundle resourceBundle;
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		resourceBundle=ResourceBundle
+				.getBundle("db");
+		String userName=resourceBundle.getString("userName");
+		String password=resourceBundle.getString("password");
+		String url=resourceBundle.getString("url");
+		String driver=resourceBundle.getString("driverClassName");
+		//load the driver
+		Class.forName(driver);
+		return DriverManager.getConnection(url, userName, 
+				password);
+		
+		
+		
+	}
+
+}
