f569c6aca859fe649170ddb6bbc2f165661720a3
eswaribala <parameswaribala@gmail.com>
2023-08-29 16:19:09 +0530

    updated

--- retailerapp/src/main/java/com/retailer/dao/CarDao.java
+++ retailerapp/src/main/java/com/retailer/dao/CarDao.java
@@ -9,4 +9,7 @@ public interface CarDao {
 
 	boolean addCar(Car car) throws ClassNotFoundException, SQLException;
     List<Car> getAllCars() throws ClassNotFoundException, SQLException ;
+    Car getCarByModelName(String modelName)throws ClassNotFoundException, SQLException ;
+    Car updateCarSeatingCapacity(Car car)throws ClassNotFoundException, SQLException ;
+    boolean deleteCar(String modelName)throws ClassNotFoundException, SQLException ;
 }
--- retailerapp/src/main/java/com/retailer/dao/CarDaoImpl.java
+++ retailerapp/src/main/java/com/retailer/dao/CarDaoImpl.java
@@ -74,4 +74,76 @@ public class CarDaoImpl implements CarDao {
 		return cars;
 	}
 
+	@Override
+	public Car getCarByModelName(String modelName) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		//step 2 get connection
+		conn=MySQLHelper.getConnection();
+		String query=this.resourceBundle.getString("getcarbymodelname");
+		Car car=null;
+	    try{
+					//step3 create statement
+		  this.statement=this.conn.prepareStatement(query);
+		  this.statement.setString(1, modelName);
+		  this.resultSet=this.statement.executeQuery();
+		  this.resultSet.next();
+		  car=new Car(this.resultSet.getString(1),
+				  this.resultSet.getByte(2));
+	    }
+	    finally
+	    {
+	    	//step5 
+	    	conn.close();
+	    }
+		return car;
+	}
+
+	@Override
+	public Car updateCarSeatingCapacity(Car car) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		//step 2 get connection
+		
+		conn=MySQLHelper.getConnection();
+		int rows=0;
+		String query=this.resourceBundle.getString("updatecar");
+		try{
+			//step3 create statement
+			this.statement=this.conn.prepareStatement(query);
+			this.statement.setString(2, car.getModelName());
+			this.statement.setByte(1, car.getSeatingCapacity());
+			//step 4 execute the statement and get results
+			rows=this.statement.executeUpdate();
+						
+		}
+		finally {
+			//step 5 close the connection
+			conn.close();
+		}
+		return getCarByModelName(car.getModelName());
+	}
+
+	@Override
+	public boolean deleteCar(String modelName) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		conn=MySQLHelper.getConnection();
+		int rows=0;
+		String query=this.resourceBundle.getString("deletecar");
+		try{
+			//step3 create statement
+			this.statement=this.conn.prepareStatement(query);
+			this.statement.setString(1, modelName);			
+			//step 4 execute the statement and get results
+			rows=this.statement.executeUpdate();						
+		}
+		finally {
+			//step 5 close the connection
+			conn.close();
+		}
+		if(rows>0)
+	 	 return true;
+		else
+			return false;
+	}
+
 }
