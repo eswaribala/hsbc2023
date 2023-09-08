4f91f3592f2a0295f361db6da34dac6907e24eea
eswaribala <parameswaribala@gmail.com>
2023-09-08 16:31:51 +0530

    updated

--- ecommerceapi/ecommerceapi/src/main/java/com/hsbc/ecommerceapi/controllers/CustomerController.java
+++ ecommerceapi/ecommerceapi/src/main/java/com/hsbc/ecommerceapi/controllers/CustomerController.java
@@ -1,6 +1,10 @@
 package com.hsbc.ecommerceapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hsbc.ecommerceapi.dtos.ResponseWrapper;
 import com.hsbc.ecommerceapi.models.Customer;
 import com.hsbc.ecommerceapi.repositories.CustomerRepository;
 import com.hsbc.ecommerceapi.services.CustomerService;
@@ -19,21 +24,28 @@ import com.hsbc.ecommerceapi.services.CustomerService;
 @RequestMapping("/customers")
 public class CustomerController {
 	@Autowired
-	private CustomerService customerService;
-	
-	
-	public ResponseEntity<> addCustomer(@RequestBody Customer customer){
+	private CustomerService customerService;	
+	@PostMapping("/v1.0/")
+	public ResponseEntity<ResponseWrapper> addCustomer(@RequestBody Customer customer){
 	
 		Customer customerObj=this.customerService.addCustomer(customer);
 		if(customerObj!=null) {
-			
+			return ResponseEntity.status(HttpStatus.OK).
+					body(new ResponseWrapper(customerObj));	
 		}
 		else
 		{
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+					body(new ResponseWrapper("Customer not saved..."));
 			
 		}
 		
 	}
+	@GetMapping("/v1.0/")
+	public List<Customer> getAllCustomers(){
+		return this.customerService.getCustomers();
+	}
+	
 	
 	
 }
