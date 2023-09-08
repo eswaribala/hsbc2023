f07bacf3db686817e52bccd67adc9eaa41443ce9
eswaribala <parameswaribala@gmail.com>
2023-09-08 14:11:42 +0530

    updated

--- ecommerceappv1/ecommerceappv1/src/main/java/com/hsbc/ecommerceappv1/controllers/HomeController.java
+++ ecommerceappv1/ecommerceappv1/src/main/java/com/hsbc/ecommerceappv1/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.hsbc.ecommerceappv1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.hsbc.ecommerceappv1.models.Customer;
+import com.hsbc.ecommerceappv1.repositories.CustomerRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private CustomerRepository customerRepository;
+	
 	//url handler
 	@GetMapping("/home")
 	public String home(Model model) {
@@ -19,10 +24,19 @@ public class HomeController {
 		return "home.html";
 	}
     @PostMapping("/register")
-	public String register(@ModelAttribute("customer") Customer customer) {
-		
+	public String register(@ModelAttribute("customer") Customer customer,
+			Model model) {
+		int rows=0;
     	if(customer.getFirstName()!=null)
-    		return "success.html";
+    	{
+    	    rows=customerRepository.addCustomer(customer);
+    	    if(rows>0) {
+    	      model.addAttribute("customerName", customer.getFirstName());	
+    		  return "success.html";
+    	    }
+    	    else
+    	      return "redirect:/home";	
+    	}
     	else
     		return "redirect:/home";
 	}
