5622c7ae728fd083a7032bf910ee406dd2595cf8
eswaribala <parameswaribala@gmail.com>
2023-09-13 15:37:06 +0530

    updated

--- reactprojects/bankapp/src/App.js
+++ reactprojects/bankapp/src/App.js
@@ -15,20 +15,24 @@ function BankApp() {
 //export default BankApp;
 //life cycle methods
 export class BankApp extends Component{
-
-
     constructor(props, context) {
         super(props, context);
+        console.log("Entering  constructor");
     }
-
     componentWillUnmount() {
-        super.componentWillUnmount();
+        //super.componentWillUnmount();
+        console.log("Entering  will mount");
     }
-
     render() {
+        console.log("Entering  render");
+        return(
+            <div>
+                <h1>HSBC Bank</h1>
+            </div>
+        )
     }
-
     componentDidMount() {
-        super.componentDidMount();
+        //super.componentDidMount();
+        console.log("Entering  did mount");
     }
 }
