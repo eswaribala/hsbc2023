22ea786eb263058f8e24ae32fd5dba6b829951d7
eswaribala <parameswaribala@gmail.com>
2023-09-13 16:19:41 +0530

    updated

--- reactprojects/bankapp/src/App.js
+++ reactprojects/bankapp/src/App.js
@@ -18,7 +18,20 @@ export class BankApp extends Component{
     constructor(props, context) {
         super(props, context);
         console.log("Entering  constructor");
+        //step 1 create state
+        this.state={
+            currentTime:new Date()
+        }
+
     }
+    //step2
+    timerEvent=()=>{
+        this.setState({
+            currentTime:new Date()
+        })
+    }
+
+
     componentWillUnmount() {
         //super.componentWillUnmount();
         console.log("Entering  will mount");
@@ -26,13 +39,17 @@ export class BankApp extends Component{
     render() {
         console.log("Entering  render");
         return(
-            <div>
+            <div className="App-header">
                 <h1>HSBC Bank</h1>
+                <h5>{this.state.currentTime.toLocaleString()}</h5>
             </div>
         )
     }
     componentDidMount() {
         //super.componentDidMount();
         console.log("Entering  did mount");
+        //step 3 call once in a second
+        setInterval(this.timerEvent,1000);
+
     }
 }
