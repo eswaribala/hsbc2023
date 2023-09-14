8e28c9e93db91750044fafaf0f95e2902400f511
eswaribala <parameswaribala@gmail.com>
2023-09-14 09:13:24 +0530

    updated

---
+++ reactprojects/bankapp/src/models/menudata.js
@@ -0,0 +1,59 @@
+export const items=[
+    {
+        label: 'Banking',
+        icon: 'pi pi-fw pi-file',
+        items: [
+            {
+                label: 'CreditCards',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {
+                        label: 'Cashback Card',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Visa Platinum Card',
+                        icon: 'pi pi-fw pi-video'
+                    },
+                    {
+                        label: 'Smart Value Card',
+                        icon: 'pi pi-fw pi-video'
+                    },
+                    {
+                        label: 'Compare Card',
+                        icon: 'pi pi-fw pi-video'
+                    },
+                ]
+            },
+            {
+                label: 'CreditCard Features',
+                icon: 'pi pi-fw pi-trash'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Bank Accounts',
+                icon: 'pi pi-fw pi-external-link',
+                items:[
+                    {
+                        label: 'Savings Account',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+
+                    {
+                        label: 'Fixed Deposit',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Debit Card',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                ]
+            }
+        ]
+    }
+
+
+
+]
