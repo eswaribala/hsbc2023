4e20b3f8dac0d12c76b10fdea5d237f4aad10f1c
eswaribala <parameswaribala@gmail.com>
2023-09-15 10:29:55 +0530

    updated

--- reactprojects/bankapp/src/SavingsAccount/savingsaccount.js
+++ reactprojects/bankapp/src/SavingsAccount/savingsaccount.js
@@ -7,6 +7,10 @@ import { Calendar } from 'primereact/calendar';
 import { Dropdown } from 'primereact/dropdown';
 import './savingsaccount.css'
 import {validate} from "./validator";
+import axios from "axios";
+
+let restAPIUrl="http://localhost:7070/customers/v1.0/"
+
 //functional approach
 //React Hook 16.8
 function SavingsAccount(props) {
@@ -64,8 +68,32 @@ function SavingsAccount(props) {
         setErrors(response.errorsValue);
         console.log(errors);
         let isValid=response.status;
+        if(isValid) {
+            setIsAddDisabled(false);
+           // inputs.dob = inputs.dob.toLocaleDateString('en-ZA');
+            let convertedDate = inputs.dob.getFullYear()
+                + "-" +((inputs.dob.getMonth()+1).length != 2 ? "0" +
+                    (inputs.dob.getMonth() + 1) : (inputs.dob.getMonth()+1))
+                + "-" + (inputs.dob.getDate().length != 2 ?"0" +
+                    inputs.dob.getDate() : inputs.dob.getDate());
 
-    }
+           // let convertedDate= inputs.dob.getFullYear()+"-"+inputs.dob.getMonth()+"-"+inputs.dob.getDay();
+            inputs.dob=convertedDate;
+            axios.post(restAPIUrl, inputs).then(res => {
+                console.log(res);
+                setFirstName("")
+                setLastName("")
+                setDOB("")
+                setAddress("")
+                setContactNo(0)
+                setGender("")
+                setEmail("")
+            }).catch(error => {
+                throw(error);
+            });
+        }
+
+}
 
 
 
@@ -74,79 +102,79 @@ function SavingsAccount(props) {
 
 
 
-    return(
-            <div>
-                <form onSubmit={handleSubmit} className="form border border-danger shadow-none p-3 rounded">
-                    <Fieldset legend="Customer Form">
-                    <span className="mt-5">
-                        <label htmlFor="firstName">First Name</label>
-                        <InputText name="firstName" type="text" pattern="[a-zA-Z]*" value={firstName} onChange={handleOnChange}  className="form-control"/>
-                        {errors.firstName &&
-                        <div style={{ color: "red", paddingBottom: 10 }}>
-                            {errors.firstName}</div>
-                        }
-                    </span>
-                        <span className="mt-5">
-                        <label htmlFor="lastName" className="form-label">Last Name</label>
-                        <InputText name="lastName" type="text" pattern="[a-zA-Z]*" value={lastName} onChange={handleOnChange} className="form-control"/>
-                        {errors.lastName &&
-                            <div style={{ color: "red", paddingBottom: 10 }}>
-                                {errors.lastName}</div>
-                        }
-                    </span>
-                        <span className="mt-5">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <InputText name="email" type="email" value={email} onChange={handleOnChange} className="form-control"/>
-                        {errors.email &&
-                        <div style={{ color: "red", paddingBottom: 10 }}>
-                            {errors.email}</div>
-                        }
-                    </span>
-                        <span className="mt-5">
-                         <label htmlFor="dob" className="form-label">DOB</label>
-                         <Calendar  name="dob" value={dob} className="w-100" onChange={handleOnChange}
-                                    dateFormat="yy/mm/dd"  touchUI />
+return(
+ <div>
+     <form onSubmit={handleSubmit} className="form border border-danger shadow-none p-3 rounded">
+         <Fieldset legend="Customer Form">
+         <span className="mt-5">
+             <label htmlFor="firstName">First Name</label>
+             <InputText name="firstName" type="text" pattern="[a-zA-Z]*" value={firstName} onChange={handleOnChange}  className="form-control"/>
+             {errors.firstName &&
+             <div style={{ color: "red", paddingBottom: 10 }}>
+                 {errors.firstName}</div>
+             }
+         </span>
+             <span className="mt-5">
+             <label htmlFor="lastName" className="form-label">Last Name</label>
+             <InputText name="lastName" type="text" pattern="[a-zA-Z]*" value={lastName} onChange={handleOnChange} className="form-control"/>
+             {errors.lastName &&
+                 <div style={{ color: "red", paddingBottom: 10 }}>
+                     {errors.lastName}</div>
+             }
+         </span>
+             <span className="mt-5">
+             <label htmlFor="email" className="form-label">Email</label>
+             <InputText name="email" type="email" value={email} onChange={handleOnChange} className="form-control"/>
+             {errors.email &&
+             <div style={{ color: "red", paddingBottom: 10 }}>
+                 {errors.email}</div>
+             }
+         </span>
+             <span className="mt-5">
+              <label htmlFor="dob" className="form-label">DOB</label>
+              <Calendar  name="dob" value={dob} className="w-100" onChange={handleOnChange}
+                         dateFormat="yy/mm/dd"  touchUI />
 
-                            {errors.dob &&
-                            <div style={{ color: "red", paddingBottom: 10 }}>
-                                {errors.dob}</div>
-                            }
-                    </span>
-                    <span className="mt-5">
-                         <label htmlFor="contactNo" className="form-label">Contact No</label>
-                        <InputText name="contactNo" type="number" value={contactNo} onChange={handleOnChange} className="form-control"/>
-                        {errors.contactNo &&
-                        <div style={{ color: "red", paddingBottom: 10 }}>
-                            {errors.contactNo}</div>
-                        }
-                    </span>
-                    <span className="mt-5">
-                        <label htmlFor="gender" className="form-label">Gender</label>
-                       <Dropdown name="gender" value={gender}
-                                 options={genderList} onChange={handleOnChange}
-                                 placeholder="Select a Gender" className="form-select"/>
-                        {errors.gender &&
-                        <div style={{ color: "red", paddingBottom: 10 }}>
-                            {errors.gender}</div>
-                        }
-                    </span>
-                        <span className="mt-5">
-                          <label htmlFor="address" className="form-label">Address</label>
-                         <InputTextarea name="address" rows={5} cols={30} value={address}
-                                      autoResize  onChange={handleOnChange} className="form-control"/>
-                        {errors.address &&
-                        <div style={{ color: "red", paddingBottom: 10 }}>
-                            {errors.address}</div>
-                        }
-                    </span>
-                       <span className="mt-5">
-                         <Button label="Submit" className="mt-3 form-control p-button-rounded p-button-danger"
-                            disabled={!isAddDisabled}  />
-                        </span>
-                    </Fieldset>
-                </form>
-            </div>
-        )
+                 {errors.dob &&
+                 <div style={{ color: "red", paddingBottom: 10 }}>
+                     {errors.dob}</div>
+                 }
+         </span>
+         <span className="mt-5">
+              <label htmlFor="contactNo" className="form-label">Contact No</label>
+             <InputText name="contactNo" type="number" value={contactNo} onChange={handleOnChange} className="form-control"/>
+             {errors.contactNo &&
+             <div style={{ color: "red", paddingBottom: 10 }}>
+                 {errors.contactNo}</div>
+             }
+         </span>
+         <span className="mt-5">
+             <label htmlFor="gender" className="form-label">Gender</label>
+            <Dropdown name="gender" value={gender}
+                      options={genderList} onChange={handleOnChange}
+                      placeholder="Select a Gender" className="form-select"/>
+             {errors.gender &&
+             <div style={{ color: "red", paddingBottom: 10 }}>
+                 {errors.gender}</div>
+             }
+         </span>
+             <span className="mt-5">
+               <label htmlFor="address" className="form-label">Address</label>
+              <InputTextarea name="address" rows={5} cols={30} value={address}
+                           autoResize  onChange={handleOnChange} className="form-control"/>
+             {errors.address &&
+             <div style={{ color: "red", paddingBottom: 10 }}>
+                 {errors.address}</div>
+             }
+         </span>
+            <span className="mt-5">
+              <Button label="Submit" className="mt-3 form-control p-button-rounded p-button-danger"
+                 disabled={!isAddDisabled}  />
+             </span>
+         </Fieldset>
+     </form>
+ </div>
+)
 
 }
 export default SavingsAccount;
